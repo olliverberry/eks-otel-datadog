04a9e6cf4f61ad0f5c2d98a86878bd7aface0c35
stuartcmehrens <stuart.mehrens@datadoghq.com>
2024-02-23 19:13:24 -0500

    update install script

--- scripts/install-eksctl.sh
+++ scripts/install-eksctl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-ARCH=$(uname -m)
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
@@ -10,6 +11,7 @@ curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_ch
     | grep $PLATFORM \
     | sha256sum --check
 
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp \
+    && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
